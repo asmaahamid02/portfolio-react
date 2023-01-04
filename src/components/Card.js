fb6d82d53d79c5a8e9403081da05796b4caadd12
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-01-04 22:30:54 +0200

    frontend => added Card component

---
+++ src/components/Card.js
@@ -0,0 +1,36 @@
+import { Heading, HStack, Image, Text, VStack } from '@chakra-ui/react'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Card = ({ title, description, imageSrc, gitHubRepoLink }) => {
+  console.log(description)
+  return (
+    <VStack
+      backgroundColor='white'
+      color='black'
+      borderRadius='lg'
+      alignItems='flex-start'
+      maxWidth='lg'
+      maxHeight='lg'
+      pb={4}
+    >
+      <Image src={imageSrc} alt={title} borderRadius='lg' />
+      <Heading as='h3' size='md' pl={4}>
+        {title}
+      </Heading>
+      <Text fontSize='sm' pl={4}>
+        {description}
+      </Text>
+      <a href={gitHubRepoLink} target='_blank' rel='noreferrer'>
+        <HStack>
+          <Text fontSize='xs' pl={4} as="b">
+            View on GitHub
+          </Text>
+          <FontAwesomeIcon icon={faArrowRight} size='xs' />
+        </HStack>
+      </a>
+    </VStack>
+  )
+}
+
+export default Card
