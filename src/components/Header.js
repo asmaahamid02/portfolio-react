cd02939ac29e7195daeb3ad2491655db7d2c4078
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-01-05 18:29:08 +0200

    frontend => added scroll function to the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import {
   faGithub,
   faLinkedin,
-  faMedium,
   faStackOverflow,
 } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useEffect, useRef, useState } from 'react'
 
 const socials = [
   {
@@ -28,38 +28,57 @@ const socials = [
 ]
 
 const Header = () => {
+  const headerRef = useRef()
+  const [prevScrollPos, setPrevScrollPos] = useState(0)
+
   const handleClick = (anchor) => {
     const id = `${anchor}-section`
     const element = document.getElementById(id)
     element.scrollIntoView({ behavior: 'smooth', block: 'start' })
   }
 
+  useEffect(() => {
+    const handleScroll = (e) => {
+      //show/hide header on scroll
+      const currentScrollPos = window.pageYOffset
+      if (currentScrollPos >= prevScrollPos || currentScrollPos === 0) {
+        headerRef.current.style.transform = 'translateY(0)'
+      } else {
+        headerRef.current.style.transform = 'translateY(-100%)'
+      }
+      setPrevScrollPos(currentScrollPos)      
+    }
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [prevScrollPos])
   return (
     <Box
       position='fixed'
       top={0}
       left={0}
       right={0}
-      translateY={0}
+      translateY={200}
       transitionProperty='transform'
       transitionDuration='0.3s'
       transitionTimingFunction='ease-in-out'
       backgroundColor='#18181b'
+      ref={headerRef}
     >
-      <Box
-      color='white'
-      maxWidth='1280px'
-      margin='0 auto'
-      >
+      <Box color='white' maxWidth='1280px' margin='0 auto'>
         <HStack
-        px={16}
-        py={4}
-        justifyContent='space-between'
-        alignItems='center'
+          px={16}
+          py={4}
+          justifyContent='space-between'
+          alignItems='center'
         >
           <nav>
             {/* Add social media links based on the `socials` data */}
-            <HStack spacing={8}>
+            {/*add anchor to HStack */}
+
+            <HStack spacing={8} >
               {socials.map((social, index) => {
                 return (
                   <a key={index} href={social.url} target='_blank'>
@@ -72,8 +91,8 @@ const Header = () => {
           <nav>
             <HStack spacing={8}>
               {/* Add links to Projects and Contact me section */}
-              <a onClick={() => handleClick('projects')}>Projects</a>
-              <a onClick={() => handleClick('contactme')}>Contact Me</a>
+              <button onClick={() => handleClick('projects')}>Projects</button>
+              <button onClick={() => handleClick('contactme')}>Contact Me</button>
             </HStack>
           </nav>
         </HStack>
