447b528acba8616494eb540f8dae338e2ab77bb3
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-01-05 13:50:30 +0200

    frontend => added alert provider

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import ContactMeSection from './components/ContactMeSection';
-import Header from './components/Header';
-import LandingSection from './components/LandingSection';
-import ProjectSection from './components/ProjectsSection';
+import { ChakraProvider } from '@chakra-ui/react'
+import ContactMeSection from './components/ContactMeSection'
+import Header from './components/Header'
+import LandingSection from './components/LandingSection'
+import ProjectSection from './components/ProjectsSection'
+import { AlertProvider } from './context/AlertContext'
+import Alert from './components/Alert'
 
 function App() {
   return (
     <ChakraProvider>
-      <main>
-        <Header />
-        <LandingSection />
-        <ProjectSection />
-        <ContactMeSection />
-      </main>
+      <AlertProvider>
+        <main>
+          <Header />
+          <LandingSection />
+          <ProjectSection />
+          <ContactMeSection />
+          <Alert />
+        </main>
+      </AlertProvider>
     </ChakraProvider>
-  );
+  )
 }
 
-export default App;
+export default App
